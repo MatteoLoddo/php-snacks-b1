67c6c60157b267d2844918836525e6be32ea970b
MatteoLoddo <loddomatteo96@gmail.com>
2022-07-08 16:02:06 +0200

    completed 1 milestone

--- snak1.php
+++ snak1.php
@@ -1,23 +1,49 @@
 <?php
-$partita = [
-    "casa" => "",
-    "ospite" => "",
+$team = [ 
+    "Oklahoma City Thunder",
+    "Charlotte Hornets",
+    "Chicago Bulls",
+    "Cleveland Cavaliers",
+    "Denver Nuggets",
+    "Milwaukee Bucks",
+    "Washington Wizards",
+    "Oklahoma City Thunder",
+    "Portland Trail Blazers",
+    "Utah Jazz",
 ];
-$campionato = [
+$team2 = [ 
+    "Indiana Pacers",
+    "Los Angeles Lakers	",
+    "San Antonio Spurs",
+    "Boston Celtics",
+    "Miami Heat",
+    "Sacramento Kings",
+    "Orlando Magic",
+    "Philadelphia 76ers",
+    "Toronto Raptors",
+    "Phoenix Suns",
+    ];
+
+$partita = [
 ];
 
+$campionato = [];
+
+// creo un ciclo che mi pushi l array $partita nell array $campionato
 do {
     $listNumber = [];
+
 // genero 2 numeri casuali e li pusho in un array vuoto
 for($i=0 ; $i < 2 ; $i++){
-    $randomNumber = random_int(0, 10);
+    $randomNumber = rand(0, 10);
     $listNumber[] = $randomNumber;
 }
 // dichairo le mie due key nell array partita utilizzando la lista dei numeri generati random
 $partita["casa"] = $listNumber[0];
-$partita["ospite"] = $listNumber[1]; 
-$campionato[] = $partita;
+$partita["ospite"] = $listNumber[1];
 
+$campionato[] = $partita;
+// ciclo che si ripete finche la lunghezza di $campionato non e pari a 10
 }while (count($campionato) < 10);
 
 
@@ -26,7 +52,8 @@ $campionato[] = $partita;
 
 
 
-var_dump( $campionato, )
+// var_dump( $campionato, );
+
 
 
 ?>
@@ -40,8 +67,13 @@ var_dump( $campionato, )
     <title>Document</title>
 </head>
 <body>
-    
-
-    
+    <?php for($i = 0 ; $i < count($campionato) ; $i++){?>
+    <div>
+        <span><?php echo $team[$i] . " - "?>  </span>
+        <span><?php echo $team2[$i] . " | "?> </span>
+        <span><?php echo ($campionato[$i]["casa"]) . " - "?>  </span>
+        <span><?php echo ($campionato[$i]["ospite"])?></span>
+    </div>
+    <?php } ?>
 </body>
 </html>
